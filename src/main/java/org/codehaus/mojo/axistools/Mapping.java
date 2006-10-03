2c0a64ecc7abe7bd8c68e1fd92163aff0e62ae9c
Dan Tran <dantran@gmail.com>
2006-10-03 02:24:31 +0000

    MOJO-501: add a null inherited Mapping.java to make plugin configuration happy
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/axistools-maven-plugin@2468 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/axistools/Mapping.java
@@ -0,0 +1,32 @@
+package org.codehaus.mojo.axistools;
+
+/*
+ * Copyright 2005 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This class is here in order to allow configuration of mapping elements in the
+ * WSDL2JavaMojo configuration. Maven looks for the Mapping class in the same
+ * package as the Mojo, so we just duplicate that class here.
+ * 
+ * @author mlaurie
+ * 
+ */
+
+public class Mapping 
+    extends org.codehaus.mojo.axistools.axis.Mapping 
+{
+
+}
