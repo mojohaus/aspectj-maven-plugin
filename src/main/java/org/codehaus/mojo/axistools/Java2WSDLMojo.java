cebeca5acaa9d8598a23890c674b9322d352a15b
David J. M. Karlsen <david@davidkarlsen.com>
2008-12-19 14:13:57 +0000

    MAXISTOOLS-38: Improve documentation
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/axistools-maven-plugin@8373 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/axistools/Java2WSDLMojo.java
+++ src/main/java/org/codehaus/mojo/axistools/Java2WSDLMojo.java
@@ -45,18 +45,19 @@ public class Java2WSDLMojo
     extends AbstractMojo
 {
     /**
-     * the directory the compile objects will be located for java2wsdl to source from
-     *
+     * The directory the compile objects will be located for java2wsdl to source from.
      * @parameter expression="${project.build.outputDirectory}"
      */
     private File classesDirectory;
 
     /**
+     * Directory for generated content.
      * @parameter expression="${project.build.directory}/generated-sources/axistools/java2wsdl"
      */
     private File outputDirectory;
 
     /**
+     * Indicates the name of the output WSDL file.
      * @parameter expression="${fileName}"
      * @required
      */
@@ -68,21 +69,32 @@ public class Java2WSDLMojo
     private String classOfPortType;
 
     /**
+     * Optional parameter that indicates the name of the input wsdl file. 
+     * The output wsdl file will contain everything from the input wsdl file plus the new constructs. 
+     * If a new construct is already present in the input wsdl file, it is not added. 
+     * This option is useful for constructing a wsdl file with multiple ports, bindings, or portTypes.
      * @parameter expression="${input}"
      */
     private String input;
 
     /**
+     * Indicates the url of the location of the service. 
+     * The name after the last slash or backslash is the name of the service port 
+     * (unless overridden by the servicePortName option). 
+     * The service port address location attribute is assigned the specified value.
      * @parameter expression="${location}"
      */
     private String location;
 
     /**
+     * Indicates the name to use for the portType element. If not specified, the {@linkplain #classOfPortType} name is used.
      * @parameter expression="${portTypeName}"
      */
     private String portTypeName;
 
     /**
+     * Indicates the name to use for the binding element. 
+     * If not specified, the value of the {@linkplain #servicePortName} + "SoapBinding" is used.
      * @parameter expression="${bindingName}"
      */
     private String bindingName;
@@ -93,11 +105,14 @@ public class Java2WSDLMojo
     private String serviceElementName;
 
     /**
+     * Indicates the name of the service port. 
+     * If not specified, the service port name is derived from the location value. 
      * @parameter expression="${servicePortName}"
      */
     private String servicePortName;
 
     /**
+     * Indicates the name of the target namespace of the WSDL.
      * @parameter expression="${namespace}"
      */
     private String namespace;
@@ -108,76 +123,110 @@ public class Java2WSDLMojo
     private String packageToNamespace;
 
     /**
+     * Methods to export.
      * @parameter expression="${methods}"
      */
     private ArrayList methods;
 
     /**
+     * Look for allowed methods in inherited class.
      * @parameter expression="false"
      */
     private boolean all;
 
     /**
+     * Indicates the kind of WSDL to generate. Accepted values are:
+     * <ul>
+     *   <li>All --- (default) Generates wsdl containing both interface and implementation WSDL constructs.
+     *   <li>Interface --- Generates a WSDL containing the interface constructs (no service element).
+     *   <li>Implementation -- Generates a WSDL containing the implementation. The interface WSDL is imported via the {@linkplain #locationImport} option.
+     * </ul>
      * @parameter expression="${outputWSDLMode}"
      */
     private String outputWSDLMode;
 
     /**
+     * Used to indicate the location of the interface WSDL when generating an implementation WSDL.
      * @parameter expression="${locationImport}"
      */
     private String locationImport;
 
     /**
+     * Namespace of the implementation WSDL.
      * @parameter expression="${namespaceImpl}"
      */
     private String namespaceImpl;
 
     /**
+     * Use this option to indicate the name of the output implementation WSDL file. 
+     * If specified, Java2WSDL will produce interface and implementation WSDL files. 
+     * If this option is used, the {@linkplain #outputWSDLMode} option is ignored.
      * @parameter expression="${outputImpl}"
      */
     private String outputImpl;
 
     /**
+     * Sometimes extra information is available in the implementation class file. 
+     * Use this option to specify the implementation class.
      * @parameter expression="${implClass}"
      */
     private String implClass;
 
     /**
+     * List of methods not to export.
      * @parameter expression="${exclude}"
      */
     private ArrayList excludes;
 
     /**
+     * List of classes which stop the Java2WSDL inheritance search.
      * @parameter expression="${stopClasses}"
      */
     private ArrayList stopClasses;
 
     /**
+     * Choose the default type mapping registry to use. Either 1.1 or 1.2.
      * @parameter expression="${typeMappingVersion}"
      */
     private String typeMappingVersion;
 
     /**
+     * The value of the operations soapAction field. 
+     * Values are DEFAULT, OPERATION or NONE. 
+     * OPERATION forces soapAction to the name of the operation. 
+     * DEFAULT causes the soapAction to be set according to the operation's meta data (usually ""). 
+     * NONE forces the soapAction to "". The default is DEFAULT.
      * @parameter expression="${soapAction}"
      */
     private String soapAction;
 
     /**
+     * The style of the WSDL document: RPC, DOCUMENT or WRAPPED. 
+     * The default is RPC. If RPC is specified, an rpc wsdl is generated. 
+     * If DOCUMENT is specified, a document wsdl is generated. 
+     * If WRAPPED is specified, a document/literal wsdl is generated using the wrapped approach. 
+     * Wrapped style forces the use attribute to be literal.
      * @parameter expression="${style}"
      */
     private String style;
 
     /**
+     * The use of the WSDL document: LITERAL or ENCODED. 
+     * If LITERAL is specified, the XML Schema defines the representation of the XML for the request. 
+     * If ENCODED is specified, SOAP encoding is specified in the generated WSDL.
      * @parameter expression="${use}"
      */
     private String use;
 
     /**
+     * Specify a list of class names which should be included in the types section of the WSDL document. 
+     * This is useful in the case where your service interface references a base class and you would like your WSDL to contain XML Schema type definitions for these other classes. 
      * @parameter expression="${extraClasses}"
      */
     private ArrayList extraClasses;
 
     /**
+     * A file or URL to an XML Schema that should be physically imported into the generated WSDL.
      * @parameter expression="${importSchema}"
      */
     private String importSchema;
