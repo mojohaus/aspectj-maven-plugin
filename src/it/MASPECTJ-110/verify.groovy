2ae4dce9ad9fb18ea6c00b6ee221d06cdc823373
Lennart Jörelid <lj@jguru.se>
2013-12-03 13:23:16 +0000

    Fix for MASPECTJ-127, including a Windows compatibility fix.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/aspectj-maven-plugin@18943 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/MASPECTJ-110/verify.groovy
+++ src/it/MASPECTJ-110/verify.groovy
@@ -1,6 +1,9 @@
 log = new File( basedir, 'build.log' )
 assert log.exists()
 
+// Ensure that we can run this test on a windows box.
+sep = File.separator == '\\' ? '\\\\' : File.separator
+
 List<String> logFileLines = log.readLines();
 println ' ==> Got [' + logFileLines.size() + '] lines in the LogFile'
 
@@ -17,9 +20,9 @@ nonexistentAspectSourcePathDirLine = logFileLines.findAll {
 println 'Got nonexistentAspectSourcePathDirLine: ' + nonexistentAspectSourcePathDirLine
 
 assert nonexistentAspectSourcePathDirLine.size() == 3
-assert nonexistentAspectSourcePathDirLine.get(0).contains( 'src/main/nonexistentCompileDirectory]' )
-assert nonexistentAspectSourcePathDirLine.get(1).contains( 'src/main/trivialaspects]' )
-assert nonexistentAspectSourcePathDirLine.get(2).contains( 'src/main/aspect]' )
+assert nonexistentAspectSourcePathDirLine.get(0).contains( 'src/main/nonexistentCompileDirectory]'.replaceAll('/', sep) )
+assert nonexistentAspectSourcePathDirLine.get(1).contains( 'src/main/trivialaspects]'.replaceAll('/', sep) )
+assert nonexistentAspectSourcePathDirLine.get(2).contains( 'src/main/aspect]'.replaceAll('/', sep) )
 
 // 2) The three non-existent test source roots *should not* be added to the testCompileSourceRoot list:
 //
@@ -34,9 +37,9 @@ nonexistentTestAspectSourcePathDirLine = logFileLines.findAll {
 println  'Got nonexistentTestAspectSourcePathDirLine: ' + nonexistentTestAspectSourcePathDirLine
 
 assert nonexistentTestAspectSourcePathDirLine.size() == 3
-assert nonexistentTestAspectSourcePathDirLine.get(0).contains( 'src/test/aspect]' )
-assert nonexistentTestAspectSourcePathDirLine.get(1).contains( 'src/test/aspect]' )
-assert nonexistentTestAspectSourcePathDirLine.get(2).contains( 'src/test/nonexistentTestCompileDirectory]' )
+assert nonexistentTestAspectSourcePathDirLine.get(0).contains( 'src/test/aspect]'.replaceAll('/', sep) )
+assert nonexistentTestAspectSourcePathDirLine.get(1).contains( 'src/test/aspect]'.replaceAll('/', sep) )
+assert nonexistentTestAspectSourcePathDirLine.get(2).contains( 'src/test/nonexistentTestCompileDirectory]'.replaceAll('/', sep) )
 
 // 3) The properly existing source root *should* be added to the compileSourceRoot list:
 //
@@ -49,7 +52,7 @@ existingAspectSourcePathDir = logFileLines.findAll {
 println 'Got existingAspectSourcePathDir: ' + existingAspectSourcePathDir
 
 assert existingAspectSourcePathDir.size() == 1
-assert existingAspectSourcePathDir.get(0).contains( 'src/main/trivialaspects]' )
+assert existingAspectSourcePathDir.get(0).contains( 'src/main/trivialaspects]'.replaceAll('/', sep) )
 
 // 4) The properly existing test source root *should* be added to the testCompileSourceRoot list:
 //
@@ -62,7 +65,7 @@ existingTestAspectSourcePathDir = logFileLines.findAll {
 println 'Got existingTestAspectSourcePathDir: ' + existingTestAspectSourcePathDir
 
 assert existingTestAspectSourcePathDir.size() == 1
-assert existingTestAspectSourcePathDir.get(0).contains( 'src/test/trivialtestaspects]' )
+assert existingTestAspectSourcePathDir.get(0).contains( 'src/test/trivialtestaspects]'.replaceAll('/', sep) )
 
 // 5) Finally, ensure that the compileSourceRoots and testCompileSourceRoots do not contain
 //    any non-existent directories
@@ -75,13 +78,13 @@ resultingCompileSourceRoots = logFileLines.find {
   line -> line.contains( '[echo] [compileSourceRoots]' )
 }
 println "Got resultingCompileSourceRoots: " + resultingCompileSourceRoots
-assert resultingCompileSourceRoots.contains( 'src/main/java' )
-assert resultingCompileSourceRoots.contains( 'src/main/trivialaspects' )
+assert resultingCompileSourceRoots.contains( 'src/main/java'.replaceAll('/', sep) )
+assert resultingCompileSourceRoots.contains( 'src/main/trivialaspects'.replaceAll('/', sep) )
 
 resultingTestCompileSourceRoots = logFileLines.find {
   line -> line.contains( '[echo] [testCompileSourceRoots]' )
 }
 
 println "Got resultingTestCompileSourceRoots: " + resultingTestCompileSourceRoots
-assert resultingTestCompileSourceRoots.contains( 'src/test/java' )
-assert resultingTestCompileSourceRoots.contains( 'src/test/trivialtestaspects' )
+assert resultingTestCompileSourceRoots.contains( 'src/test/java'.replaceAll('/', sep) )
+assert resultingTestCompileSourceRoots.contains( 'src/test/trivialtestaspects'.replaceAll('/', sep) )
